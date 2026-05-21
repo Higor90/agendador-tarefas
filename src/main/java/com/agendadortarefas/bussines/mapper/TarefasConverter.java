20110d5c858f7da4cb30aab465e30b7c790df236
Higor <nobregahigor@gmail.com>
2026-05-21 20:21:21 -0300

    adicionados os metódos get por email e por período

--- src/main/java/com/agendadortarefas/bussines/mapper/TarefasConverter.java
+++ src/main/java/com/agendadortarefas/bussines/mapper/TarefasConverter.java
@@ -3,11 +3,20 @@ package com.agendadortarefas.bussines.mapper;
 import com.agendadortarefas.bussines.dto.TarefasDTO;
 import com.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "Spring")
 public interface TarefasConverter {
-
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataEvento", target = "dataEvento")
+    @Mapping(source = "dataCriacao", target = "dataCriacao")
     TarefasEntity paraTarefasEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefasDto(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dto);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }
